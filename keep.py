ac8ab5fbdd368e8f2b98e0fadfc53ae0d23a883e
Dist Bit <adp01@pm.me>
2024-02-03 18:54:06 +0300

    notes pull from keep is working

--- keep.py
+++ keep.py
@@ -1,5 +1,4 @@
 import gkeepapi
-import json
 import utils.general as general
 
 
@@ -12,23 +11,20 @@ def fetch_notes_text():
 
     gnotes = list(keep.find(archived=False, trashed=False))
     # Extract text from each note and compile into a newline-separated string
-    seenNotes = json.load(open(general.getAbsPath("../seenNotes.json")))
-    notes_text = "\n\n" + "\n\n".join(
-        note.text for note in gnotes if note.text and note.id not in seenNotes
-    )
+    notes_text = "\n\n"
+    for note in gnotes:
+        if note.text or note.title:
+            notes_text += note.title + "\n" if note.title else ""
+            notes_text += note.text if note.text else ""
+            notes_text += "\n\n"
+
+    notes_text = notes_text.replace("\n\n\n", "\n\n")
     if notes_text.strip():
         with open(general.getConfig()["tempNotesPath"], "a") as f:
             f.write(notes_text)
 
-    for note in gnotes:
-        if note.id not in seenNotes:
-            seenNotes.append(note.id)
-
-    json.dump(seenNotes, open(general.getAbsPath("../seenNotes.json"), "w"))
-
     for gnote in gnotes:
         gnote.archived = True
-        gnote._archived = True
 
     keep.sync()
 
