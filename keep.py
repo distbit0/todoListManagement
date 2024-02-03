d48d5c382fdcac4555420d26068ac68426300179
Dist Bit <adp01@pm.me>
2024-02-03 18:41:52 +0300

    with manual gnote deduping logic

--- keep.py
+++ keep.py
@@ -1,4 +1,5 @@
 import gkeepapi
+import json
 import utils.general as general
 
 
@@ -9,12 +10,21 @@ def fetch_notes_text():
         general.getConfig()["masterKey"],
     )
 
-    gnotes = keep.find(archived=False, trashed=False)
+    gnotes = list(keep.find(archived=False, trashed=False))
     # Extract text from each note and compile into a newline-separated string
-    notes_text = "\n\n" + "\n\n".join(note.text for note in gnotes if note.text)
+    seenNotes = json.load(open(general.getAbsPath("../seenNotes.json")))
+    notes_text = "\n\n" + "\n\n".join(
+        note.text for note in gnotes if note.text and note.id not in seenNotes
+    )
     if notes_text.strip():
         with open(general.getConfig()["tempNotesPath"], "a") as f:
             f.write(notes_text)
+
+    for note in gnotes:
+        if note.id not in seenNotes:
+            seenNotes.append(note.id)
+
+    json.dump(seenNotes, open(general.getAbsPath("../seenNotes.json"), "w"))
 
     for gnote in gnotes:
         gnote.archived = True
