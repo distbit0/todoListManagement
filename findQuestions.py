3d2f1f75df3a3ab4555f52067c50eaf5b7c547f9
Dist Bit <adp01@pm.me>
2024-03-16 15:04:52 +0300

    excludedQuestions.json findQuestions.py

--- findQuestions.py
+++ findQuestions.py
@@ -1,9 +1,25 @@
 import re
 import random
 import gkeepapi
+import glob
 import utils.general as general
 
 markAsReadString = "[[read]]"
+
+
+def checkParagraphSatisfiesConstraints(paragraph, matchingWords):
+    validParagraph = not (
+        "])" in paragraph or "http" in paragraph or paragraph.endswith(markAsReadString)
+    )
+    containsMatchingWords = (
+        any(word.lower() + " " in paragraph.lower() for word in matchingWords)
+        or not matchingWords
+    )
+    paragraphSansLinks = re.sub(r"\[\[[^\]]*\]\]|#\w+", "", paragraph).strip()
+    paragraphSansLinks = paragraphSansLinks.replace("  ", " ").replace("  ", " ")
+    hasSufficientWords = len(paragraphSansLinks.split()) >= 6
+
+    return validParagraph and containsMatchingWords and hasSufficientWords
 
 
 def find_random_matches(file_list, matchingWords, num_matches):
@@ -20,20 +36,7 @@ def find_random_matches(file_list, matchingWords, num_matches):
             )  ##split on two or more newlines
             for i in range(1, len(paragraphs)):
                 currentParagraph = paragraphs[i].strip()
-                validParagraph = not (
-                    "])" in paragraphs[i]
-                    or "http" in paragraphs[i]
-                    or currentParagraph.endswith(markAsReadString)
-                )
-                containsMatchingWords = (
-                    any(
-                        word.lower() + " " in currentParagraph.lower()
-                        for word in matchingWords
-                    )
-                    or not matchingWords
-                )
-                hasSufficientWords = len(currentParagraph.split()) >= 6
-                if containsMatchingWords and validParagraph and hasSufficientWords:
+                if checkParagraphSatisfiesConstraints(currentParagraph, matchingWords):
                     matches.append(currentParagraph)
     print("number of total matches: " + str(len(matches)))
     # Randomize order of the matches list
@@ -76,52 +79,70 @@ def mark_as_read(file_list, sentences):
             continue
 
 
-# List of files to search through
-file_list = general.getConfig()["questionFiles"]
-# Number of random matches to choose
-num_matches = 15
-# Call the function to find random matches from the files
-questions = find_random_matches(
-    file_list,
-    ["when", "why", "how", "does", "what", "is", "where", "which", "could", "couldn't"],
-    num_matches,
-)
-statements = find_random_matches(file_list, [], num_matches)
+def getFileList():
+    # List of files to search through
+    file_list = general.getConfig()["questionFiles"]
+    ## append all files in ["todoFolderPath"] which end with "?.md"
+    questionFiles = [
+        path
+        for path in glob.glob(
+            general.getConfig()["toDoFolderPath"] + "**/*[?].md", recursive=True
+        )
+    ]
+    file_list += questionFiles
+    return file_list
 
-keep = gkeepapi.Keep()
-keep.resume(
-    general.getConfig()["username"],
-    general.getConfig()["masterKey"],
-)
-gnotes = list(keep.find(archived=False, trashed=False))
-questionsNote = next((note for note in gnotes if note.title == "Questions"), None)
-statementsNote = next((note for note in gnotes if note.title == "Statements"), None)
 
-# Create notes if they don't exist
-if not questionsNote:
-    questionsNote = keep.createList("Questions", [])
-if not statementsNote:
-    statementsNote = keep.createList("Statements", [])
+def main():
+    # Number of random matches to choose
+    num_matches = 15
+    # Call the function to find random matches from the files
+    file_list = getFileList()
+    questions = find_random_matches(
+        file_list,
+        general.getConfig()["questionWords"],
+        num_matches,
+    )
+    statements = find_random_matches(file_list, [], num_matches)
 
-# Get checked questions and mark them as read in the source files
-checked_sentences = []
-for item in questionsNote.items:
-    if item.checked:
-        checked_sentences.append(item.text)
-for item in statementsNote.items:
-    if item.checked:
-        checked_sentences.append(item.text)
-mark_as_read(file_list, checked_sentences)
+    keep = gkeepapi.Keep()
+    keep.resume(
+        general.getConfig()["username"],
+        general.getConfig()["masterKey"],
+    )
+    gnotes = list(keep.find(archived=False, trashed=False))
+    questionsNote = next((note for note in gnotes if note.title == "Questions"), None)
+    statementsNote = next((note for note in gnotes if note.title == "Statements"), None)
 
-for item in questionsNote.items:
-    item.delete()
-for item in statementsNote.items:
-    item.delete()
+    # Create notes if they don't exist
+    if not questionsNote:
+        questionsNote = keep.createList("Questions", [])
+    if not statementsNote:
+        statementsNote = keep.createList("Statements", [])
 
-# Add new items to the list
-for question in questions:
-    questionsNote.add(question, False)
-for statement in statements:
-    statementsNote.add(statement, False)
+    # Get checked questions and mark them as read in the source files
+    checked_sentences = []
+    for item in questionsNote.items:
+        if item.checked:
+            checked_sentences.append(item.text)
+    for item in statementsNote.items:
+        if item.checked:
+            checked_sentences.append(item.text)
+    mark_as_read(file_list, checked_sentences)
 
-keep.sync()
+    for item in questionsNote.items:
+        item.delete()
+    for item in statementsNote.items:
+        item.delete()
+
+    # Add new items to the list
+    for question in questions:
+        questionsNote.add(question, False)
+    for statement in statements:
+        statementsNote.add(statement, False)
+
+    keep.sync()
+
+
+if __name__ == "__main__":
+    main()
