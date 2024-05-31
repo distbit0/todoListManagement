2ebfffd2a611fbef90785ad76ee302d0aac91c55
Dist Bit <adp01@pm.me>
2024-05-31 18:19:29 +0400

    delete auto wikilink functionality because it is made redundant by my vimm commands

--- utils/formatting.py
+++ utils/formatting.py
@@ -4,43 +4,6 @@ import re
 ## in order to sort todos with deps to end of sublist, move them down to just above the next todo which is less indented than them, and only if the todo below it doest also have a dep (because this would indicate that it has already been moved down previously)
 
 
-def extractToDoText(todoSegment):
-    # Check if the text contains any of the specified patterns
-    if (
-        "[[" in todoSegment
-        or "]]" in todoSegment
-        or "](" in todoSegment
-        or "!(" in todoSegment
-        or "://" in todoSegment
-    ):
-        return None
-
-    todoSegment = (
-        todoSegment.replace("- [ ] ", "").replace("- [x] ", "").replace("- [/] ", "")
-    ).strip()
-
-    # Check if the text starts with "dep " (case-insensitive) and contains a colon
-    if todoSegment.lower().startswith("dep ") and ":" in todoSegment:
-        todoSegment = todoSegment.split(":", 1)[1].strip()
-
-    # Define the regex patterns
-    patterns = [r"\s*#(\d+|[a-zA-Z])", r"@(\d+)", r"\^(\d{1,2}/\d{1,2})"]
-
-    # Find the earliest occurrence of any of the patterns
-    earliest_match_index = float("inf")
-    for pattern in patterns:
-        match = re.search(pattern, todoSegment)
-        if match and match.start() < earliest_match_index:
-            earliest_match_index = match.start()
-
-    # If any patterns are found, cut the text from the earliest occurrence
-    if earliest_match_index != float("inf"):
-        return todoSegment[:earliest_match_index].strip()
-
-    # If no patterns are found, return the entire text
-    return todoSegment.strip()
-
-
 def formatTodoSegmentDepencency(todoSegment):
     if "] dep " in todoSegment.lower() and ":" in todoSegment:
         todoSegment = todoSegment.replace("] dep ", "] (DEP) ")
@@ -55,10 +18,6 @@ def formatTodoSegment(todoSegment, hasChild):
     )
     if not hasCheckbox or hasChild:
         return todoSegment
-    # only do this once I can prevent files created by foam from replacing spaces with dashes
-    # todoText = extractToDoText(todoSegment)
-    # if todoText != None and len(todoText) > 0 and len(todoText) < 55:
-    # todoSegment = todoSegment.replace(todoText, f"[[{todoText}]]")
     todoSegment = formatTodoSegmentDepencency(todoSegment)
     return todoSegment
 
