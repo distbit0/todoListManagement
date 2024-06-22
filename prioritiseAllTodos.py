a6c51d6ed79807979fde90d898f17f719f2b9cf5
Dist Bit <adp01@pm.me>
2024-06-22 13:21:39 +0100

    prioritiseAllTodos.py

--- prioritiseAllTodos.py
+++ prioritiseAllTodos.py
@@ -135,10 +135,7 @@ def createNoteFromTodo(todoPaths, path, priority, autoCreate=False):
         return todoPaths
     if not os.path.exists(newNotePath):
         with open(newNotePath, "w") as f:
-            if autoCreate:
-                f.write("")
-            else:
-                f.write(oldTodoName)
+            f.write(oldTodoName)
         print("created new note: {}".format(newNotePath))
     indexOfPath = todoPaths.index(path)
     linkToNewNote = f"[[{newFileName.replace('.md', '')}]]"
